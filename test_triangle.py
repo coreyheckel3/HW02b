40835abafb216489e66462b8ad4363bc563e8f50
coreyheckel3 <coreyheckel3@gmail.com>
2023-11-07 21:27:08 -0500

    Mutpy
    Mutpy testing

--- test_triangle.py
+++ test_triangle.py
@@ -20,6 +20,9 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleA(self): 
         self.assertEqual(classify_triangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
+    def testMutantA(self):
+        self.assertEqual(classify_triangle(8,5,3), 'NotATriangle', 'Not a triangle')
+
     def testRightTriangleB(self): 
         self.assertEqual(classify_triangle(5,3,4),'Right','5,3,4 is a Right triangle')
 
@@ -45,13 +48,23 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classify_triangle(200,200,200), 'Equilateral', '200,200,200 should be Equilateral')
 
     def testInvalidInputB(self):
-        self.assertEqual(classify_triangle(0,0,0), 'NotATriangle', '0,0,0 should be invalid')
+       self.assertEqual(classify_triangle(0,0,0), 'NotATriangle', '0,0,0 should be invalid')
 
     def testInvalidInputC(self):
         self.assertEqual(classify_triangle(1,2,1.2), 'InvalidInput', '1,2,1.2 should be invalid')
 
     def testNotTriangle(self):
         self.assertEqual(classify_triangle(1,2,3), 'NotATriangle', '1,2,3 should not be a triangle')
+
+    def testRightTriangleC(self):
+        self.assertEqual(classify_triangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+
+    def testMutantB(self):
+        self.assertEqual(classify_triangle(1, 1, 2), 'NotATriangle', '1,1,2 is not a triangle')
+    def testMutantC(self):
+        self.assertEqual(classify_triangle(2,4,2), 'NotATriangle', 'not a triangle')
+
+
 
 if __name__ == '__main__':
     print('Running unit tests')
